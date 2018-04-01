60ba9ab18a038979691a18de5e186230da5249ba
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-04-01 00:38:57 -0400

    Header done; working on footer

---
+++ src/main/resources/static/js/centigrade.js
@@ -0,0 +1,14 @@
+var $header = $("#header");
+var $nav = $("nav");
+
+console.log($header.height());
+
+$(window).scroll(function() {
+    var scrolled = $(window).scrollTop();
+    
+    if (scrolled > ($header.height() - $nav.height())) {
+        $nav.css("background", "rgba(0,0,0,0.75)");
+    } else {
+        $nav.css("background", "transparent");
+    }
+});
