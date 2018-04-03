d77c22816c8f6493dda65e62821a2c93cbd67ee3
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-04-02 23:09:04 -0400

    Register.html with base javascript

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,10 @@
+console.log("hello");
+function validateForm(form) {
+    var formName = document.forms[form]["displayname"].value;
+    console.log(form);
+    if (form == "signupform") {
+        var alert = document.getElementsByClassName("alert");
+        console.log(alert[0]);
+        alert[0].style.display = "block";
+    }
+}
