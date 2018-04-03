63a1c6f84750e5c5dfafc3b228ed8cee670852dc
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-04-02 23:43:25 -0400

    Added carousel for index.html

--- src/main/resources/static/js/centigrade.js
+++ src/main/resources/static/js/centigrade.js
@@ -9,6 +9,9 @@ $(window).scroll(function() {
     if (scrolled > ($header.height() - $nav.height())) {
         $nav.css("background", "rgba(0,0,0,0.75)");
     } else {
-        $nav.css("background", "transparent");
+        $nav.css("background", "rgba(100, 100, 100, 0.4)");
     }
 });
+
+$('.carousel').carousel();
+$('.carousel').carousel('pause');
