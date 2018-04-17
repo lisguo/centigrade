70c4307dee962e863e6bf0838cb271173f0e9b23
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-04-17 14:39:08 -0400

    Added nav bar fixes

--- src/main/resources/static/js/centigrade.js
+++ src/main/resources/static/js/centigrade.js
@@ -1,12 +1,28 @@
 var $header = $("#header");
 var $nav = $("nav");
 
-console.log($header.height());
+var pathname = window.location.pathname;
+console.log(pathname);
+
+switch (pathname) {
+    case "/": // home
+        $nav.css("position", "fixed");
+        break;
+    case "/login":
+    case "/register":
+        break;
+    default:
+        $nav.css("position", "-webkit-sticky");
+        $nav.css("position", "sticky");
+        $nav.css("background", "black");
+        $nav.css("top", "0px");
+        break;
+}
 
 $(window).scroll(function () {
     var scrolled = $(window).scrollTop();
 
-    if (scrolled > ($header.height() - $nav.height())) {
+    if (scrolled > ($header.height() - $nav.height()) && pathname === "/") {
         $nav.css("background", "rgba(0,0,0,0.75)");
     } else {
         $nav.css("background", "rgba(100, 100, 100, 0.4)");
