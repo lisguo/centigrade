f802ce7cc083f0111948b65f8566bb71b548406e
Vanessa Ho <poyiu.ho@stonybrook.edu>
2018-04-18 03:08:18 -0400

    Slight changes to movie.html

--- src/main/resources/static/js/centigrade.js
+++ src/main/resources/static/js/centigrade.js
@@ -12,20 +12,19 @@ switch (pathname) {
     case "/register":
         break;
     default:
-        $nav.css("position", "-webkit-sticky");
-        $nav.css("position", "sticky");
-        $nav.css("background", "black");
-        $nav.css("top", "0px");
+        $nav.css("position", "relative");
+        $nav.css("background", "rgba(0,0,0, 0.75)");
         break;
 }
 
 $(window).scroll(function () {
     var scrolled = $(window).scrollTop();
-
-    if (scrolled > ($header.height() - $nav.height()) && pathname === "/") {
-        $nav.css("background", "rgba(0,0,0,0.75)");
-    } else {
-        $nav.css("background", "rgba(100, 100, 100, 0.4)");
+    if (pathname === "/") {
+        if (scrolled > ($header.height() - $nav.height())) {
+            $nav.css("background", "rgba(0,0,0,0.75)");
+        } else {
+            $nav.css("background", "rgba(100, 100, 100, 0.4)");
+        }
     }
 });
 
