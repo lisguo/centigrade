63b7fb916ee7f18a1268764e0ac61590e9c415fc
lisguo <lisa.guo@stonybrook.edu>
2018-05-10 12:34:49 -0400

    Added ajax in index page. All sections are now in separate template files under index_

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    loadlatestCriticReviews();
+    loadMostPopularMovies();
+    loadOpeningSoon();
+    loadTopBoxOffice();
+});
+
+function loadTopBoxOffice(){
+    $.ajax({
+        type : 'GET',
+        url : '/index_top_box_office'
+    }).done(function(data) {
+        $('#index-top-box-office').html(data);
+    });
+}
+
+function loadOpeningSoon(){
+    $.ajax({
+        type : 'GET',
+        url : '/index_opening_soon'
+    }).done(function(data) {
+        $('#index-opening-soon').html(data);
+    });
+}
+
+function loadMostPopularMovies(){
+    $.ajax({
+        type : 'GET',
+        url : '/index_most_popular_movies'
+    }).done(function(data) {
+        $('#index-most-popular-movies').html(data);
+    });
+}
+
+function loadlatestCriticReviews(){
+    $.ajax({
+        type : 'GET',
+        url : '/index_latest_critic_reviews'
+    }).done(function(data) {
+        $('#index-latest-critic-reviews').html(data);
+    });
+}
